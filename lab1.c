a5f275852794f7002c4d5c0640d8afecd8861179
Алымбаев Тимур Русланович <ip612s01@mwp08.csc.local>
2017-02-16 11:24:25 +0700

    Ubral probel, dobavil peremenn k dlya hraneniya sroka

--- lab1.c
+++ lab1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-
 int main()
 {
 	int n;
+	int k;
 	printf("Vvedite summu vklada");
     scanf("%d", &n);
-	
+    printf("Vvedite srok");
+	scanf("%d", &k);
+  
 return 0;
 }
